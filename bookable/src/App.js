9877ed309fda8472e043557ff3886225535aa606
Patech_Patrice <patricedrayton86@gmail.com>
2022-04-07 13:12:18 -0400

    created category containers to seperate book genres

--- bookable/src/App.js
+++ bookable/src/App.js
@@ -1,87 +1,61 @@
-// import logo from './logo.svg'
 import './App.css';
-import axios from 'axios';
-import Books from './components/books';
-import Users from './components/users';
+// import axios from 'axios';
+// import Books from './components/books';
+// import Users from './components/users';
 import Dashboard from './components/dashboard';
 import Authentication from './components/auth/authentication';
 import { useState, useEffect } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-
-// const API_URL = "http://localhost:3000/api/v1/books";
-
-function getBookData(){
-  return axios.get('/api/v1/books').then((response) => response.data)
-}
-
-function getUserData(){
-  return axios.get('/api/v1/users').then((response) => response.data)
-}
+import './categories.styles.scss';
+import CategoryItem from './components/category-item/category-item.js';
 
 
 
-function App() {
-  const [books, setBooks] = useState([]);
-  const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    let mounted = true;
-    getBookData().then((items) =>  {
-      if (mounted) {
-        setBooks(items);
-      }
-  });
-  return () => {(mounted = false)};
-}, []);
+// import HomePage from 'bookable/src/components/homepage/homepage.js';
+const App = () => {
 
-useEffect(() => {
-  let mounted = true;
-  getUserData().then((items) =>  {
-    if (mounted) {
-      setUsers(items);
-    }
-});
-return () => {(mounted = false)};
-}, []);
+
+  const categories = [
+    {
+    id: 1,
+    title: 'Horror Novels',
+    image: require('./assets/images/horror_logo.png'),
+  },
+  {
+    id: 2,
+    title: ' Suspense Novels',
+    image: require('./assets/images/suspense_logo.jpeg')
+  },
+  {
+    id: 3,
+    title: 'Mystery Novels',
+    image: require('./assets/images/mystery_logo.jpg'),
+  },
+  {
+    id: 4,
+    title: 'Thrillers Novels',
+    image: require('./assets/images/thriller_logo.png'), 
+  },
+  {
+    id: 5,
+    title: 'Romance Novels',
+    image: require('./assets/images/romance_logo.png'),
+  }
+]
 
   return (
-    <div className="App">
-      <nav>
-      <ul>
-      <li>
-           <Link to="authentication">Log In or Sign Up</Link>
-          </li>
-          <li>
-           <Link to="/">Home</Link>
-          </li>
-          <li>
-           <Link to="users">Users</Link>
-          </li>
-          <li>
-            <Link to="dashboard">Dashboard</Link>
-         </li>
-          <li>
-           <Link to="books">Books</Link>
-          </li>
-        </ul>
-        </nav>
 
+        <div className="categories-container">
+           {categories.map(( category) =>(
+          
+              <CategoryItem key={category.id} category={category} />
+               ))}  
 
-        
-                  <div className="main">
-                          <Routes>
-                            <Route path="books" element={<Books books={books}/>}></Route>
-                            <Route path="users" element={<Users users={users}/>}></Route>
-                            <Route path="dashboard" element={<Dashboard/>}></Route>
-                            <Route path="authentication" element={<Authentication/>}></Route>
-                      
-                          </Routes>
-                          </div>
-  
-    </div>
+            
+          </div>
   );
-}
-
+};
 
 
 export default App;
